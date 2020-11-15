7b443279b1a18045e5f5bafdc3eabd635895d3a0
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-15 11:02:08 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan40-WarnaKepribadian/src/pbo2/pkg10116417/latihan40/warnakepribadian/User.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan40.warnakepribadian;
+
+/**
+ *
+ *
+ */
+class User {
+    private String nama, warna;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    
+}
